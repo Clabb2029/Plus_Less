8fc258cb8d87146a3023c14b3f79ec8fa36da911
Clara Slys <slys.clara89000@gmail.com>
2022-06-06 02:04:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const PlusDownDiv = ({ id, onDelete }) => {
+
+  const [childrenComp, setChildrenComp] = useState([])
+
+  const removeChildById = (childId) => {
+    setChildrenComp(childrenComp.filter((childData) => childData.id !== childId));
+  };
+
+  const addChild = () => {
+    const childrenIds = childrenComp.map((child) => child.id);
+    const lastChildrenId = Math.max(0, ...childrenIds);
+    setChildrenComp([...childrenComp, { id: lastChildrenId + 1 }]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ marginLeft: 10, marginTop:10, fontSize: 20 }}>
+      
+      <div>
+        - {id}
+        <button style={{marginLeft: 5, marginRight: 5}} onClick={() => {addChild()}}> + </button>
+        <button onClick={() => {onDelete()}}> - </button>
+      </div>
+
+      {childrenComp.map((childData) => (
+        <PlusDownDiv
+          id={childData.id}
+          key={childData.id}
+          onDelete={() => {
+            removeChildById(childData.id);
+          }}
+        />
+      ))}
+
+    </div>
+  )
+};
+
+
+const App = () => {
+  return (
+    <div>
+      <PlusDownDiv id={1} onDelete={() => null} />
     </div>
   );
-}
+};
+
 
 export default App;
